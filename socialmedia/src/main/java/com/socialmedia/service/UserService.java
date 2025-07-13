cefd8b7ddb003f021e1be46d1f97a0b23033ad18
Mahima Rajput <rajputmahima909@gmail.com>
2025-07-13 18:56:43 +0530

    Added UserService interface

---
+++ socialmedia/src/main/java/com/socialmedia/service/UserService.java
@@ -0,0 +1,7 @@
+package com.socialmedia.service;
+
+import com.socialmedia.model.User;
+
+public interface UserService {
+    User registerUser(User user);
+}
---
+++ socialmedia/src/main/java/com/socialmedia/service/UserServiceImplement.java
@@ -0,0 +1,4 @@
+package com.socialmedia.service;
+
+public class UserServiceImplement {
+}

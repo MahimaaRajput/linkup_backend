e978db3806abeced0f43a3e02c9530daa423f487
Mahima Rajput <rajputmahima909@gmail.com>
2025-07-15 17:57:22 +0530

    Added ApiResponse class for custom API messages

---
+++ socialmedia/src/main/java/com/socialmedia/response/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.socialmedia.response;
+
+public class ApiResponse {
+    private String message;
+    private Boolean status;
+
+    public ApiResponse(String message, Boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+    public ApiResponse()
+    {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
